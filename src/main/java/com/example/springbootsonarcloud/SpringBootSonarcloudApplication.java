fa91e06119f5912ff9bdc5e0fcc46daecbb95d20
Mantas Matūzas <11620454+mantasmatuzas@users.noreply.github.com>
2023-01-27 11:17:56 +0200

    Initial commit (2)

---
+++ src/main/java/com/example/springbootsonarcloud/SpringBootSonarcloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsonarcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSonarcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSonarcloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootsonarcloud/SpringBootSonarcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsonarcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSonarcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
