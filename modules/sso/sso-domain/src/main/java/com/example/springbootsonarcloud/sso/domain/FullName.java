e0588385a6782de4377d4732ca5de63e04e0aace
Mantas Matūzas <11620454+mantasmatuzas@users.noreply.github.com>
2023-01-27 21:27:00 +0200

    Add code duplication

--- modules/sso/sso-domain/src/main/java/com/example/springbootsonarcloud/sso/domain/FullName.java
+++ modules/sso/sso-domain/src/main/java/com/example/springbootsonarcloud/sso/domain/FullName.java
@@ -4,4 +4,25 @@ public record FullName(String firstName, String lastName) {
     public String asString() {
         return String.format("%s %s", firstName, lastName);
     }
+
+    public String asString1() {
+        return String.format("%s %s", firstName, lastName);
+    }
+
+    public String asString2() {
+        return String.format("%s %s", firstName, lastName);
+    }
+
+    public String asString3() {
+        return String.format("%s %s", firstName, lastName);
+    }
+
+    public String asString4() {
+        return String.format("%s %s", firstName, lastName);
+    }
+
+    public String asString5() {
+        return String.format("%s %s", firstName, lastName);
+    }
+
 }
